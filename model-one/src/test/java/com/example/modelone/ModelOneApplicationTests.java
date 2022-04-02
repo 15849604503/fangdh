7c1c0423e73103ee0d2ff7ce4645057c5e4f4fe5
fangdh <750331510@qq.com>
2022-04-02 16:25:04 +0800

    构建项目

---
+++ model-one/src/test/java/com/example/modelone/ModelOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modelone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
